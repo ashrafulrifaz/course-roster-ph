7bfb27a828ffb20edc89930b47a83317f78f2eb3
ashrafulrifaz <developerashrafulislamrifaz@gmail.com>
2023-09-14 20:59:20 +0600

    fake json data created for course posts

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,11 @@ import './App.css'
 function App() {
 
   return (
-    <>
-      
-    </>
+    <div className='max-w-6xl mx-auto'>
+      <div className='py-5'>
+        <h1 className='text-center text-3xl font-bold'>Course Registration</h1>
+      </div>      
+    </div>
   )
 }
 
